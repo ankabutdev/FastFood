31c198548315152c092a2d8088c83d414d7f8de3
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-07-21 10:23:53 +0500

    Added products to the interface and added ISerachable to Common

---
+++ Common/ISearchable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FastFood.Common;
+
+public interface ISearchable<TModel>
+{
+    public Task<(int ItemCount, IList<TModel>)> SearchAsync(string search);
+}
---
+++ Interfaces/Products/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using FastFood.Common;
+using FastFood.Entites.Products;
+using FastFood.ViewModels.Products;
+
+namespace FastFood.Interfaces.Products;
+
+public interface IProductRepository : IRepository<Product, ProductViewModel>,
+    IGetAll<ProductViewModel>, ISearchable<ProductViewModel>
+{
+}
