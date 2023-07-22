33f5c5370991a65075ce463dd56f0e3072c30e6c
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-07-22 14:34:57 +0500

    Improved

--- Interfaces/Orders/IOrderRepository.cs
+++ Interfaces/Orders/IOrderRepository.cs
@@ -1,15 +1,9 @@
 ﻿using FastFood.Entites.Orders;
 using FastFood.ViewModels.Orders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace FastFood.Interfaces.Orders
+namespace FastFood.Interfaces.Orders;
+
+public interface IOrderRepository : IRepository<Order, OrderViewModel>
 {
-    public interface IOrderRepository : IRepository<Order, OrderViewModel>
-    {
 
-    }
 }
