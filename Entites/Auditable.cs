83c20db2268986cc0d53b775c899365bfef82d14
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-08-09 19:40:26 +0500

    Improved

--- Entites/Auditable.cs
+++ Entites/Auditable.cs
@@ -5,5 +5,6 @@ namespace FastFood.Entites;
 public abstract class Auditable : BaseEntitiy
 {
     public DateTime CreatedAt { get; set; }
+
     public DateTime UpdatedAt { get; set; }
 }
--- ViewModels/Products/ProductViewModel.cs
+++ ViewModels/Products/ProductViewModel.cs
@@ -3,18 +3,20 @@ using System;
 
 namespace FastFood.ViewModels.Products;
 
-public class ProductViewModel 
+public class ProductViewModel : Auditable
 {
 
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string ImagePath { get; set; } = string.Empty;
-    public double UnitPrice { get; set; }
-    public long CategoryId { get; set; }
-    public long Quantity { get; set; }
 
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public string Description { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+
+    public double UnitPrice { get; set; }
+
+    public long CategoryId { get; set; }
+
+    public long Quantity { get; set; }
 
     //public short Discount { get; set; }
 
