3ebfffe4d4002ec17e455bf7d3a0fba86b3af147
ankabutdev <Ankabut571@gmail.com>
2024-02-18 03:59:54 +0500

    add and change

--- Constants/ContentConstant.cs
+++ Constants/ContentConstant.cs
@@ -1,7 +1,30 @@
-﻿namespace FastFood.Constants;
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace FastFood.Constants;
 
 public class ContentConstant
 {
-    public const string IMAGE_CONTENTS_PATH = @"C:\Users\Hp\OneDrive\Рабочий стол\clones\FastFood\Assets\Images\";
-    //public const string IMAGE_CONTENT_PATH_CATEGORY = "C:\\Users\\iddiu\\OneDrive\\Рабочий стол\\Projects\\2--OY Bootcamp RS Projects\\FastFood\\Assets\\Category\\Images\\";
+    private static readonly string IMAGE_CONTENTS_PATH;
+    private static readonly string IMAGE_ROOT_PATH = "Assets/Images";
+
+    static ContentConstant()
+    {
+        string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+
+        string[] directories = currentPath.Split(Path.DirectorySeparatorChar);
+
+        IMAGE_CONTENTS_PATH = string.Join(Path.DirectorySeparatorChar.ToString(), directories.Take(directories.Length - 3));
+    }
+
+    public static string GetImageContentsPath()
+    {
+        return IMAGE_CONTENTS_PATH + "\\";
+    }
+
+    public static string GetImageRootPath()
+    {
+        return GetImageContentsPath() + IMAGE_ROOT_PATH;
+    }
 }
