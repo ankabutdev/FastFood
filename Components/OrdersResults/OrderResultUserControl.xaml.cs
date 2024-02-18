e55e951247306111230ef442e8aaa9cc82453a71
ankabutdev <Ankabut571@gmail.com>
2024-02-19 01:11:31 +0500

    added order component

--- Components/OrdersResults/OrderResultUserControl.xaml.cs
+++ Components/OrdersResults/OrderResultUserControl.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using FastFood.Entites.Orders;
 using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace FastFood.Components.OrdersResults;
@@ -12,7 +14,43 @@ public partial class OrderResultUserControl : UserControl
     public OrderResultUserControl()
     {
         InitializeComponent();
+        DataContext = this;
     }
+
+    private int _productCount;
+
+    public int ProductCount
+    {
+        get { return _productCount; }
+        set
+        {
+            if (_productCount != value)
+            {
+                _productCount = value;
+                OnPropertyChanged(nameof(ProductCount));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        ProductCount++;
+    }
+
+    private void lblMinus_Click(object sender, RoutedEventArgs e)
+    {
+        if (ProductCount > 0)
+        {
+            ProductCount--;
+        }
+    }
+
 
     private void UserControl_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
