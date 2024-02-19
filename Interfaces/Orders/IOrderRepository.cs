3c3f81b75cf4c4717367de0260f2e0d081901805
ankabutdev <Ankabut571@gmail.com>
2024-02-20 02:05:15 +0500

    added new Methods

--- Interfaces/Orders/IOrderRepository.cs
+++ Interfaces/Orders/IOrderRepository.cs
@@ -1,10 +1,16 @@
 ﻿using FastFood.Common;
 using FastFood.Entites.Orders;
 using FastFood.ViewModels.Orders;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FastFood.Interfaces.Orders;
 
 public interface IOrderRepository : IRepository<Order, OrderViewModel>,
     IGetAll<Order>, ISearchable<OrderViewModel>
 {
+    public Task<IList<Order>> GetAllOrderByUserIdByIsPaidFalseAsync(long userId);
+
+    public Task<IQueryable<Order>> GetAllOrderByUserIdByIsPaidTrueAsync(long userId);
 }
