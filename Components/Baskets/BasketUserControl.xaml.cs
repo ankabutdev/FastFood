800af9c1cafb685f29ab08922e9f164c6d3c0c6b
ankabutdev <Ankabut571@gmail.com>
2024-03-04 00:54:55 +0500

    added and improved

--- Components/Baskets/BasketUserControl.xaml.cs
+++ Components/Baskets/BasketUserControl.xaml.cs
@@ -32,6 +32,20 @@ public partial class BasketUserControl : UserControl
         Id = order.Id;
         orderName.Content = order.Description.ToString();
         lblOrderPrice.Content = order.ProductsPrice.ToString() + " $";
-        //lblOrderQuantity.Content = order.
+        //lblOrderQuantity.Content = order
+
+        /*
+            SELECT orders.id as order_id, orders.user_id, order_details.id as order_details_id
+            FROM orders
+            FULL JOIN order_details 
+            ON orders.id=order_details.order_id;
+         */
+
+        /*
+        SELECT *
+        FROM orders
+        FULL JOIN order_details ON orders.id = order_details.id;
+
+         */
     }
 }
